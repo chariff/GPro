e8f6ba0e453846af25ace3ecd9cea1bf8dc98097
chariff <chariff.alkhassim@gmail.com>
2020-02-06 10:59:10 +0100

    long description

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ setup(
     url='https://github.com/chariff/GPro',
     packages=find_packages(),
     author='Chariff Alkhassim',
-    author_emain='chariff.alkhassim@gmail.com',
+    author_email='chariff.alkhassim@gmail.com',
     description='Preference Learning with Gaussian Processes',
+    long_description='Python implementation of a probabilistic kernel approach '
+                     'to preference learning based on Gaussian processes',
     install_requires=[
         "numpy >= 1.9.0",
         "scipy >= 0.14.0",
@@ -15,4 +17,5 @@ setup(
     ],
     license='MIT',
 )
+
 

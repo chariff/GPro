e5c9a0d6e110846337179d242f4b610b2e9064f0
chariff <chariff.alkhassim@gmail.com>
2020-08-20 14:47:33 +0200

    Version 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GPro',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/chariff/GPro',
     packages=find_packages(),
     author='Chariff Alkhassim',
